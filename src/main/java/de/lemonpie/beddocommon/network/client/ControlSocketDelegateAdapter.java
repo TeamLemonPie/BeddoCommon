f3532bb7d8725482c90c5ad3035989122a091d7a
tobias <thinkdifferent055@gmail.com>
2018-08-14 22:09:11 +0200

    Adopt common net classes to BeddoFakeFabrik

---
+++ src/main/java/de/lemonpie/beddocommon/network/client/ControlSocketDelegateAdapter.java
@@ -0,0 +1,34 @@
+package de.lemonpie.beddocommon.network.client;
+
+public class ControlSocketDelegateAdapter implements ControlSocketDelegate
+{
+	@Override
+	public void init(ControlSocket socket)
+	{
+
+	}
+
+	@Override
+	public void onConnectionEstablished()
+	{
+
+	}
+
+	@Override
+	public void onConnectionClosed()
+	{
+
+	}
+
+	@Override
+	public void startConnecting(String host, int port)
+	{
+
+	}
+
+	@Override
+	public void onConnectionFailed(Exception e, int retry)
+	{
+
+	}
+}
